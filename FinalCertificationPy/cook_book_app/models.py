1e509be82511b0b690c33ce46b08a322ee272459
Mutniy <wrobotw@mail.ru>
2024-10-01 16:01:54 +0300

    сделали предварительный макет моделий согласно тз

--- FinalCertificationPy/cook_book_app/models.py
+++ FinalCertificationPy/cook_book_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recipes(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    stages = models.TextField()
+    time_preparations = models.IntegerField()
+    img = models.ImageField(upload_to='recipes_img')
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name: str = 'Рецепт'
+        verbose_name_plural: str = 'Рецепты'
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length=100)
+    recipes = models.ManyToManyField(Recipes)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name: str = 'Категория'
+        verbose_name_plural: str = 'Категории'
