605e961d8905c4a2dd2338a0ea82410395b6b58b
Mutniy <wrobotw@mail.ru>
2024-10-01 16:37:22 +0300

    в Settings.py добавили настройки для Logger

--- FinalCertificationPy/FinalCertificationPy/settings.py
+++ FinalCertificationPy/FinalCertificationPy/settings.py
@@ -125,3 +125,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': '/log/dj_log.log'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+         },
+        'cook_book_app': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
