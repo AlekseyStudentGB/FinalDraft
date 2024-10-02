c3164f9917b2d983f03fa51be001ff0b6675bd42
Mutniy <wrobotw@mail.ru>
2024-10-02 14:28:56 +0300

    init

--- FinalCertificationPy/cook_book_app/urls.py
+++ FinalCertificationPy/cook_book_app/urls.py
@@ -1,8 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('addrecipes/', add_recipes, name='add_recipes'),
+    path('recipes/', get_recipes, name='get_recipes'),
+    path('recipe/<int:pk>', get_recipe, name='get_recipe'),
+    path('changerecipe/<int:pk>', change_recipe, name='change_recipe'),
+    path('addct', add_categories, name='add_categories'),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
