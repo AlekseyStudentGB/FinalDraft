52a58c988f7099197c0a1dc32fab1021b352ac38
Mutniy <wrobotw@mail.ru>
2024-10-02 14:27:26 +0300

    в модель рецептов добавили ключ для связи с моделью категорий

--- FinalCertificationPy/cook_book_app/models.py
+++ FinalCertificationPy/cook_book_app/models.py
@@ -3,14 +3,25 @@ from django.db import models
 # Create your models here.
 
 
-class Recipes(models.Model):
+class Categories(models.Model):
+    title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name: str = 'Категория'
+        verbose_name_plural: str = 'Категории'
+
+
+class Recipes(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     stages = models.TextField()
     time_preparations = models.IntegerField()
-    img = models.ImageField(upload_to='recipes_img')
-    author = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='recipes_img', blank=True)
+    author = models.CharField(max_length=100, blank=True)
+    categories = models.ForeignKey(Categories, blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -20,13 +31,4 @@ class Recipes(models.Model):
         verbose_name_plural: str = 'Рецепты'
 
 
-class Categories(models.Model):
-    title = models.CharField(max_length=100)
-    recipes = models.ManyToManyField(Recipes)
 
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name: str = 'Категория'
-        verbose_name_plural: str = 'Категории'
